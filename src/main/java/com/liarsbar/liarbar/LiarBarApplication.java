7655c37e5e817e3991fd00a62a0c1eea21c73d0a
HOWILL <1450656923@qq.com>
2024-12-08 12:05:49 +0800

    搭建基本spring boot 框架

---
+++ src/main/java/com/liarsbar/liarbar/LiarBarApplication.java
@@ -0,0 +1,13 @@
+package com.liarsbar.liarbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiarBarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiarBarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liarsbar/liarbar/LiarBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liarsbar.liarbar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiarBarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
