af876861f794debd4fa2023d0af522d90b593132
HOWILL <1450656923@qq.com>
2024-12-10 08:34:19 +0800

    实现了基础的前端页面

--- src/main/java/com/liarsbar/liarbar/Controller.java
+++ src/main/java/com/liarsbar/liarbar/Controller.java
@@ -1,11 +1,18 @@
 package com.liarsbar.liarbar;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
-    @RequestMapping("/fuck")
+
+    @RequestMapping("/gamepage")
     public String test() {
-        return "index";
+        return "game";
+    }
+
+    // 显示静态的 game.html 页面
+    @GetMapping("/gamepage")
+    public String showGamePage() {
+        return "forward:/game.html";  // 跳转到 static 目录下的 game.html
     }
 }
